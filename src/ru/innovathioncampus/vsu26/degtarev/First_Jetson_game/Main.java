a3010c218ac221853d74639482f9193e651558e4
retik903 <saweij265@gmail.com>
2026-02-24 18:34:21 +0300

    123456789

--- src/ru/innovathioncampus/vsu26/degtarev/First_Jetson_game/Main.java
+++ src/ru/innovathioncampus/vsu26/degtarev/First_Jetson_game/Main.java
@@ -1,22 +1,25 @@
 package ru.innovathioncampus.vsu26.degtarev.First_Jetson_game;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         String person = "\uD83E\uDDD9\u200D";
         int personLive = 3;
-        /* здесь необходимо рассказать про переполнение и про другие типы данных
-            int personLive = 2147483649; // мало ли кто-то захочет сделать ооочень много жизней
-         */
 
         String monster = "\uD83E\uDDDF\u200D";
-        int sizeBoard = 3;
+        String castle = "\uD83C\uDFF0";
+        int sizeBoard = 5;
         int personX = 1;
-        int personY = 3;
+        int personY = sizeBoard;
+
+
 
         int step = 0;
-        // \n, \t - как спец символ(упомянуть)
+        String leftBlock = "| ";
+        String rightBlock = "|";
+        String wall = "+ —— + —— + —— +";
         String gamingField = "+ —— + —— + —— +\n"
                 + "|    |    | \uD83C\uDFE0 |\n"
                 + "+ —— + —— + —— +\n"
@@ -24,6 +27,26 @@ public class Main {
                 + "+ —— + —— + —— +\n"
                 + "| " + person + " |    |    |\n"
                 + "+ —— + —— + —— +";
+        String[][] board = new String[sizeBoard][sizeBoard];
+        for (int y = 0; y < sizeBoard; y++) {
+            for (int x = 0; x < sizeBoard; x++) {
+                board[y][x] = "  ";
+            }
+        }
+
+
+        int countMonster = sizeBoard * sizeBoard - sizeBoard - 1;
+        Random r = new Random();
+        for (int i = 0; i <= countMonster; i++) {
+            board[r.nextInt(sizeBoard - 1)][r.nextInt(sizeBoard)] = monster;
+        }
+
+        int castleX = r.nextInt(sizeBoard);
+        int castleY = 0;
+
+
+        board[castleY][castleX] = castle;
+
 
         System.out.println("Привет! Ты готов начать играть в игру? (Напиши: ДА или НЕТ)");
 
@@ -32,70 +55,62 @@ public class Main {
         System.out.println("Ваш ответ:\t" + answer);
 
 
-
-//        if (answer.equals("ДА")) {
-
-//        System.out.println("Выбери сложность игры(от 1 до 5):");
-//        int difficultGame = sc.nextInt();
-//        System.out.println("Выбранная сложность:\t" + difficultGame);
-//            System.out.println("Сколько жизней будет у персонажа?");
-////            personLive = sc.nextInt(); // здесь необходимо рассказать о возможных ошибках(неверный тип, переполнение - еще раз и тд)
-//            System.out.println(output_str);
-//            System.out.println("Live:\t" + personLive + "\n");
-//
-//            System.out.println("Введите куда будет ходить персонаж(ход возможен только по вертикали и горизонтали на одну клетку;" +
-//                    "\nКоординаты персонажа - (x: " + personX + ", y: " + personY + "))");
-//            int x = sc.nextInt();
-//            int y = sc.nextInt();
-//            System.out.println(x + ", " + y);
-//
-//            // проверка
-//            if (x != personX && y != personY) {
-//                System.out.println("Неккоректный ход");
-//            } else if (Math.abs(x - personX) == 1) {
-//                personX = x;
-//                System.out.println("Ход корректный; Новые координаты: " + personX + ", " + personY);
-//            } else if (Math.abs(y - personY) == 1) {
-//                personY = y;
-//                System.out.println("Ход корректный; Новые координаты: " + personX + ", " + personY);
-//            }else {
-//                System.out.println("Координаты не изменены");
-//            }
-//
-//        } else if (answer.equals("НЕТ")) {
-//            System.out.println("Жаль, приходи еще!");
-//        } else {
-//            System.out.println("Данные введены неккоректно");
-//        }
-
         switch (answer) {
             case "ДА" -> {
+
                 System.out.println("Выбери сложность игры(от 1 до 5):");
                 int difficultGame = sc.nextInt();
                 System.out.println("Выбранная сложность:\t" + difficultGame);
-//                System.out.println("Сколько жизней будет у персонажа?");
-//            personLive = sc.nextInt(); // здесь необходимо рассказать о возможных ошибках(неверный тип, переполнение - еще раз и тд)
 
-                System.out.println(gamingField);
-                System.out.println("Количество жизней:\t" + personLive + "\n");
-                System.out.println("Введите куда будет ходить персонаж(ход возможен только по вертикали и горизонтали на одну клетку;" +
-                        "\nКоординаты персонажа - (x: " + personX + ", y: " + personY + "))");
-                int x = sc.nextInt();
-                int y = sc.nextInt();
-                System.out.println(x + ", " + y);
+                int maxStep = 2;
 
-                // проверка
-                if (x != personX && y != personY) {
-                    System.out.println("Неккоректный ход");
-                } else if (Math.abs(x - personX) == 1 || Math.abs(y - personY) == 1) {
-                    personX = x;
-                    personY = y;
-                    step++;
-                    System.out.println("Ход корректный; Новые координаты: " + personX + ", " + personY +
-                            "\nХод номер: " + step);
-                } else {
-                    System.out.println("Координаты не изменены");
+                while (true) {
+                    board[personY - 1][personX - 1] = person;
+                    for (String[] raw : board) {
+                        System.out.println(wall);
+                        for (String col : raw) {
+                            System.out.print(leftBlock + col + " ");
+                        }
+                        System.out.println(rightBlock);
+                    }
+                    System.out.println(wall);
+
+
+                    System.out.println("Количество жизней:\t" + personLive + "\n");
+
+                    System.out.println("Введите куда будет ходить персонаж(ход возможен только по вертикали и горизонтали на одну клетку;" +
+                            "\nКоординаты персонажа - (x: " + personX + ", y: " + personY + "))");
+                    int x = sc.nextInt();
+                    int y = sc.nextInt();
+                    System.out.println(x + ", " + y);
+
+                    // проверка
+                    if (x != personX && y != personY) {
+                        System.out.println("Неккоректный ход");
+                    } else if (Math.abs(x - personX) == 1 || Math.abs(y - personY) == 1) {
+                        if (board[y - 1][x - 1].equals("  ")) {
+                            board[personY - 1][personX - 1] = "  ";
+                            personX = x;
+                            personY = y;
+                            step++;
+                            System.out.println("Ход корректный; Новые координаты: " + personX + ", " + personY +
+                                    "\nХод номер: " + step);
+                        }else if (board[y - 1][x - 1].equals(castle)) {
+                            System.out.println("Вы прошли игру!");
+                            break;
+                        } else {
+                            System.out.println("Решите задачу.");
+                        }
+                    } else {
+                        System.out.println("Координаты не изменены");
+                    }
+
+                    if (personLive <= 0) {
+                        break;
+                    }
                 }
+
+                System.out.println("Закончились жизни. Итог: ...");
             }
             case "НЕТ" -> System.out.println("Жаль, приходи еще!");
             default -> System.out.println("Данные введены неккоректно");
